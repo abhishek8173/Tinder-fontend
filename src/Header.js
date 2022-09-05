70ba6919504fd7f3666373f96c9e6c60b3a37607
abhishek8173 <abhishekpandey19112000@gmail.com>
2022-09-05 23:46:19 +0530

    Header

--- src/Header.js
+++ src/Header.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import "./Header.css"
 import PersonIcon from '@mui/icons-material/Person';
+import ForumIcon from '@mui/icons-material/Forum';
 import { IconButton } from '@mui/material';
 
 function Header() {
   return (
     <div className='header'>
-        <h2>I'm a header</h2>
+
         <IconButton>
-            <PersonIcon fontSize='large' className='header__icon' />
+          <PersonIcon fontSize='large' className='header__icon' />
+        </IconButton>
+
+        <img className='header__logo'
+        src='https://www.logo.wine/a/logo/Tinder_(app)/Tinder_(app)-Flame-Logo.wine.svg' 
+        alt='' />
+
+        <IconButton>
+          <ForumIcon fontSize='large' className='header__icon' />
         </IconButton>
         
     </div>
